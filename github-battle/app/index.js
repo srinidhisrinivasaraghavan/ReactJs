beca11ffccbfedd2963bd8cc1607677ff7589cec
Srinidhi Raghavan <sraghavan@hqcorp.audiblemagic.com>
2016-08-04 12:58:01 -0700

    Added profile details

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,12 +1,58 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var USER_DATA = {
+  name: 'Srinidhi Srinivasa Raghavan',
+  username: 'srinidhisrinivasaraghavan',
+  image: 'https://avatars0.githubusercontent.com/u/8649439?v=3$s=460'
+}
+
+//pure function: gives same result for the same input values
+//f(d) = V
 var HelloWorld = React.createClass({
 	render:function(){
 		return(
-			<div>HelloWorld</div>
+			<div>Hello World from {this.props.name}</div>
 			)
 	}
-})
+});
 
-ReactDOM.render(<HelloWorld/>, document.getElementById('app'));
+
+var ProfileName = React.createClass({
+	render:function(){
+		return<div>
+			<p>{this.props.name}</p></div>
+			
+	}
+});
+
+var ProfileLink = React.createClass({
+	render:function(){
+		return(
+			<a href={"https://www.github.com/"+this.props.username}>{this.props.username}</a>
+			);
+	}
+});
+
+var ProfilePic = React.createClass({
+	render:function(){
+		return(
+			<img src={this.props.link} alt="Profile Pic" style={{height: 100, width: 100}}></img>
+			);
+	}
+});
+
+var UserProfile = React.createClass({
+	render:function(){
+		return(
+			<div>
+			<ProfilePic link={this.props.user.image}/>
+			<ProfileName name={this.props.user.name}/>
+			<ProfileLink username={this.props.user.username}/>
+			
+			</div>
+			);
+	}
+});
+
+ReactDOM.render(<UserProfile user={USER_DATA}/>, document.getElementById('app'));
